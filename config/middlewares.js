369f5518d23294cd4a49ec9868eec68c5b7c6ad4
Simon BENUREAU <simon@miloctav.fr>
2026-03-20 20:49:18 +0100

    Upgrade Strapi 5.40 + add Cloudflare R2 upload provider

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,19 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://*.r2.dev', 'https://*.cloudflareinsights.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://*.r2.dev'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => {
+  const isR2Enabled = !!env('CF_R2_BUCKET', '')
+
+  return {
+    // Cloudflare R2 upload provider (active when CF_R2_BUCKET is set)
+    ...(isR2Enabled && {
+      upload: {
+        config: {
+          provider: 'strapi-plugin-cloudflare-r2-assets',
+          providerOptions: {
+            basePath: 'uploads',
+          },
+        },
+      },
+      'cloudflare-r2-assets': {
+        enabled: true,
+      },
+    }),
+  }
+}
